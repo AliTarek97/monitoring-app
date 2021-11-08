bce5e5c100e00df9aa6459db9332b5e128edd0e1
AliTarek97 <aly_helmy2015@hotmail.com>
2021-11-08 20:02:26 +0200

    refactor: adding configurations to cronjob

---
+++ src/util/convertMapToRecord.ts
@@ -0,0 +1,7 @@
+export const convertMapToObject = (metricArguments: Map<string,string>): Record<string,string> => {
+    const newObject: Record<string,string> = {};
+    for (const [key, value] of metricArguments) {
+      newObject[key] = value;
+    }
+    return newObject;
+  };
--- src/util/cronJob.ts
+++ src/util/cronJob.ts
@@ -2,72 +2,83 @@ import { CronJob } from "cron";
 import axios from "axios";
 import { Check, CheckDocument } from "../models/check";
 import { bindUrl } from "./bindUrl";
-
+import { convertMapToObject } from "./convertMapToRecord";
+import axiosTime from "axios-time";
+axiosTime(axios);
 export const instantiateCronJob = (check: CheckDocument) => {
-    const job = new CronJob(
-        "*/5 * * * * *",
-        async function() {
-            try {
-                console.log(`You will see this message every ${check.interval} seconds`);
-                const url = bindUrl(check);
-                console.log(url);
-                const response = await axios.get(url);
-                console.log(response.status);
-            } catch (error) {
-                console.log(error.message);
-            }
-        },
-        null,
-        true,
-      );
-     return job;
+  const job = new CronJob(
+    "*/5 * * * * *",
+    async function () {
+      try {
+        console.log(
+          `You will see this message every ${check.interval} seconds`
+        );
+        const url = bindUrl(check);
+        console.log(url);
+        const response:any = await axios.get(url,{timeout: parseInt(`${check.timeout}`,10) / 10, headers: convertMapToObject(check.httpHeaders as Map<string,string>)});
+        console.log(response.status);
+        console.log(response.timing.elapsedTime);
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    null,
+    true
+  );
+  return job;
 };
-
 
 export const startCronJob = async () => {
-    
-    const checks = await Check.find({});
-    const jobMapper:Record<string,CronJob>  = {};
-        
-    for (const check of checks) {
-        console.log(check.url);
-        const job = new CronJob(
-            "*/5 * * * * *",
-            async function() {
-                try {
-                    console.log(`You will see this message every ${check.interval} seconds`);
-                    const url = bindUrl(check);
-                    console.log(url);
-                    const response = await axios.get(url);
-                    console.log(response.status);
-                } catch (error) {
-                    console.log(error.message);
-                }
-            },
-            null,
-            true,
-        );
-        jobMapper[`${check._id}`] = job;
-    }
+  const checks = await Check.find({});
+  const jobMapper: Record<string, CronJob> = {};
 
-    Object.assign(global, {jobMapper: jobMapper});
+  for (const check of checks) {
+    console.log(check.url);
+    const job = new CronJob(
+      "*/5 * * * * *",
+      async function () {
+        try {
+          console.log(
+            `You will see this message every ${check.interval} seconds`
+          );
+          const url = bindUrl(check);
+          console.log(url);
+          const response:any = await axios.get(url, {
+            timeout: parseInt(`{${check.timeout}}`) / 10  /* from sec to ms*/,
+            headers: convertMapToObject(
+              check.httpHeaders as Map<string, string>
+            ),
+          });
+          console.log(response.status);
+          console.log(response.timings.elapsedTime);
+        } catch (error) {
+          console.log(error.message);
+        }
+      },
+      null,
+      true
+    );
+    jobMapper[`${check._id}`] = job;
+  }
+
+  Object.assign(global, { jobMapper: jobMapper });
 };
 
-export const resumeJob = (check:CheckDocument) => {
-    const job:CronJob = (global as any).jobMapper[check.id];
-    job.start();
+export const resumeJob = (check: CheckDocument) => {
+  const job: CronJob = (global as any).jobMapper[check.id];
+  job.start();
 };
 
-export const stopJob = (check:CheckDocument) => {
-    const job:CronJob = (global as any).jobMapper[check.id];
-    console.log((global as any).jobMapper[check.id]);
-    job.stop();
+export const stopJob = (check: CheckDocument) => {
+  const job: CronJob = (global as any).jobMapper[check.id];
+  console.log((global as any).jobMapper[check.id]);
+  job.stop();
 };
 
-export const assignJobToCheck = (check:CheckDocument, job: CronJob) => {
-    (global as any).jobMapper[check.id] = job;
+export const assignJobToCheck = (check: CheckDocument, job: CronJob) => {
+  (global as any).jobMapper[check.id] = job;
 };
 
-export const removeJobAssignmentToCheck = (check:CheckDocument) => {
-    delete (global as any).jobMapper[check.id];
+export const removeJobAssignmentToCheck = (check: CheckDocument) => {
+  delete (global as any).jobMapper[check.id];
 };
