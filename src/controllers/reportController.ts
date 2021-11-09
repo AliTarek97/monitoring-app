7ed1f81c69b468f0e76bd93682c8dd3e32a412a8
AliTarek97 <aly_helmy2015@hotmail.com>
2021-11-09 03:48:27 +0200

    feat: adding report endpoit

---
+++ src/controllers/reportController.ts
@@ -0,0 +1,52 @@
+"use strict";
+
+import { Response } from "express";
+import { bindUrl } from "../util/bindUrl";
+import { AuthenticatedRequest } from "../middlewares/auth";
+import { Check } from "../models/check";
+import { PollingLogs } from "../models/pollingLogs";
+export const createReport = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const { checkId } = req.params;
+    const check = await Check.findById(checkId);
+    const url = bindUrl(check);
+
+    const status = (
+      await PollingLogs.find({ url }).sort({ _id: -1 }).limit(1)
+    )[0].status;
+    const documentsCount: number = await PollingLogs.countDocuments({ url });
+    const upUrls: number = await PollingLogs.countDocuments({
+      url,
+      status: true,
+    });
+    const availability: number = (upUrls / documentsCount) * 100;
+    const outages: number = await PollingLogs.countDocuments({
+      url,
+      status: false,
+    });
+    const avgResponseTime =
+      (
+        await PollingLogs.aggregate()
+          .match({ url: url })
+          .group({ _id: null, totalTimeElapsed: { $sum: "$elapsedTime" } })
+      )[0].totalTimeElapsed / documentsCount;
+
+    const uptime = (availability / 100) * avgResponseTime;
+    const downtime = (outages / 100) * avgResponseTime;
+    res.json({
+      status,
+      availability,
+      outages,
+      upUrls,
+      documentsCount,
+      uptime,
+      downtime,
+      avgResponseTime,
+    });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
---
+++ src/models/report.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+export type reportDocument = mongoose.Document & {
+    status: string;
+    availability: number,
+    outages: number,
+    downtime: number,
+    uptime: number,
+    responseTime: number,
+    history: string
+    
+};
+
+const reportsSchema = new mongoose.Schema<reportDocument>(
+    {
+        status: {type: String, required: true},
+        availability: {type: Number, required: true},
+        outages: {type: Number, required: true},
+        downtime: {type: Number, required: true},
+        uptime: {type: Number, required: true},
+        responseTime: {type: Number, required: true},
+        history: {type: String}
+    },
+    { timestamps: true },
+);
+
+export const reportsLogs = mongoose.model<reportDocument>("PollingLogs", reportsSchema);
